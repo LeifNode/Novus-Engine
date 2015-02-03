b8217a1a2c7f0080287e6fa0285887a2fbb57c16
Leif Erkenbrach <LErkenbrach@gmail.com>
2015-02-03 12:24:37 -0500

    Moved DebugFullscreenTriangle and added Sol System spreadsheet
    DebugFullscreenTriangle.hlsl was excluded by the .gitignore because of
    its location.

---
+++ Shaders/Utils/DebugFullscreenTriangle.hlsl
@@ -0,0 +1,23 @@
+//================================================================
+// Copyright (c) 2015 Leif Erkenbrach
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+// http://opensource.org/licenses/MIT)
+//================================================================
+
+#include "Common.hlsl"
+#include "Utils/FullscreenTriangle.hlsl"
+
+Texture2D<float4> textureDiffuse : register(t0);
+SamplerState texSampler : register(s0);
+
+TRIANGLE_OUT VS(uint vertexID : SV_VertexID)
+{
+	return FullscreenTriangle(vertexID);
+}
+
+float4 PS(TRIANGLE_OUT pin) : SV_Target
+{
+	//return float4(pin.TexCoord.xy, 0.0f, 1.0f);
+	return float4(textureDiffuse.Sample(texSampler, pin.TexCoord));
+}
